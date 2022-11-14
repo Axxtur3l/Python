e0be8fb1ca995bb33355afcd1330977d255d14a9
Alyssa G <agrenardo@brooklynsteamcenter.org>
2022-11-14 10:54:45 -0500

    Added Grade Calculator

---
+++ Add.py
@@ -0,0 +1,7 @@
+print('Ima add numbers for you cuz ima smart computer >:)')
+a=int(input('First Number: '))
+b=int(input('Second Number: '))
+Total=int(a + b)
+
+print('And the lie detector test determines...')
+print(int(Total))
---
+++ Grades.py
@@ -0,0 +1,16 @@
+print('Lets calculate your grades :)')
+print('')
+print('')
+g=int(input('Enter your grade: '))
+
+
+if g <= 59:
+    print('F')
+elif g >= 60 and g <= 69:
+    print('D')
+
+if g <= 79:
+    print('C')
+elif g >= 80 and g <= 89:
+    print('B')
+
