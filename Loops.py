14a0582490a6b6cd12224c2dd5ffb8821f088c14
Alyssa G <agrenardo@brooklynsteamcenter.org>
2022-11-15 10:56:53 -0500

    To Do list

---
+++ Loops.py
@@ -0,0 +1,21 @@
+# x = ''
+
+# # while x != 'stop':
+# #     print('Hello World')
+# #     x = input('Type stop to exit ')
+
+# y = 0
+
+# while y < 10:
+#     y = y + 1
+    
+#     print(y)
+
+songs = ['syxx anthem', 'maple', 'true relation']
+
+# for a in songs:
+#     print(a)
+
+
+for k in range(1, 11):
+    print(k)
---
+++ ToDo.py
@@ -0,0 +1,9 @@
+To_Do = []
+
+x = input('What would you like to add to the list? ')
+
+To_Do.append(x)
+
+print('Your To Do list contains: ')
+print(To_Do)
+
