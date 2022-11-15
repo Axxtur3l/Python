ae452ca63745992fb2927558ba49239f974926b1
Alyssa G <agrenardo@brooklynsteamcenter.org>
2022-11-15 09:32:40 -0500

    Tested tuples

---
+++ Lists.py
@@ -0,0 +1,35 @@
+my_list= [
+    "monke",
+    "banana",
+    'ooga booga',
+    'trees',
+    'sunflower'
+]
+print(my_list)
+
+my_list.append('bazinga')
+
+print(my_list)
+
+print(my_list[2])
+
+print(my_list[-1])
+
+my_list[-1]='leaf'
+
+del my_list[-1]
+
+#Tuple test
+my_tuple=(
+    'malice',
+    'shift reality',
+    '4EVER'
+)
+
+print(my_tuple)
+#Tuple is immutable therefore it cannot change
+my_tuple.append['sickandtired']
+
+del my_tuple[2]
+
+my_tuple[2]='AXXAD'
